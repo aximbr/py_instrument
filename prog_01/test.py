ba7d3e24cf0426a99b82d2ea676545efabe49fe2
Jose Leitao <jose.p.leitao@gmail.com>
2024-03-25 07:15:32 -0300

    Testes com lib NI-Visa

--- prog_01/test.py
+++ prog_01/test.py
@@ -1,9 +1,10 @@
 import pyvisa as visa
 
-
-rm = visa.ResourceManager('/usr/lib/librsvisa.so')
-#rm = visa.ResourceManager('@py')
-#rm = visa.ResourceManager('/usr/lib/x86_64-linux-gnu/libiovisa.so')
+visa.log_to_screen()
+#rm = visa.ResourceManager('/usr/lib/librsvisa.so')
+rm = visa.ResourceManager('@py')
+#rm = visa.ResourceManager('/usr/lib/x86_64-linux-gnu/libivivisa.so.7.0.0')
+#rm = visa.ResourceManager()
 print(rm)
 print(rm.list_resources('USB?*::INSTR'))
 ladybug = rm.open_resource('USB0::6669::5592::210118::1::INSTR')
