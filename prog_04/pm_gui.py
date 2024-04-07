6813f2d91e8e9d45cdc45f391e4a3ea55631e86a
Jose Leitao <jose.p.leitao@gmail.com>
2024-04-07 14:24:51 -0300

    Select frequecy from a List

--- prog_04/pm_gui.py
+++ prog_04/pm_gui.py
@@ -7,11 +7,10 @@ import random
 BLACK = "#000000"
 RED = "#e7305b"
 GREEN = "#55ff33"
-YELLOW = "#f7f5dd"
-#FONT_NAME = "RADIOLAND"
 FONT_NAME = "Ariel"
 reads = [0, 1, 3, 6, 2, 6, 8, 9]
 
+FREQS = [954, 1000, 2185, 2200, 4000, 4300]
 
 class App(tk.Tk):
     """Class for PM GUI"""
@@ -19,23 +18,28 @@ class App(tk.Tk):
         super().__init__()
         #main window
         self.title("Very Very Simple Power Meter")
-        self.config(background="white", padx=10, pady=5)
+        self.config(background="white", padx=5, pady=5, width=480, height=320)
         #Title
-        self.title_label = ttk.Label(text="Power Meter", foreground=BLACK, background="white", font=("Courrier", 30))
+        self.title_label = ttk.Label(text="Power Meter", foreground=BLACK, background="white", font=("Courrier", 26))
         self.title_label.grid(column=1, row=0)
         #Canvas
         self.canvas = tk.Canvas(width=150, height=60, background=BLACK, highlightthickness=0)
         self.read_text = self.canvas.create_text(80, 30, fill=GREEN, text="", font=(FONT_NAME, 18))
         self.canvas.grid(column=1, row=1)
-        #Info Button
-        self.info_button = ttk.Button(text="info", command=self.show_info)
-        self.info_button.grid(column=0, row=2)
+        #List Box with know frequencies
+        self.list_freq = tk.Listbox(height=3, width=5, selectmode='single')
+        self.list_freq.bind('<<ListboxSelect>>', self.callback_list_freq)
+        self.list_freq.grid(column=0, row=1, rowspan=1 )
+        #MHZ Label
+        self.label_MHz = ttk.Label(text="MHz")
+        self.label_MHz.grid(column=0, row=2)
         #Info Label
-        self.info_label = ttk.Label(text="", anchor='n', background="white", foreground="black", font=("Arial",8))
+        self.info_label = ttk.Label(text="", background="white",foreground="black", font=("Arial",8))
         self.info_label.grid(column=1,row=3)
         #Read Button
         self.read_button = ttk.Button(text="", command=self.start_stop_read)
-        self.read_button.grid(column=2, row=2)
+        self.read_button.grid(column=2, row=1)
+        #End of UI definition
         #Create PM
         self.pm_frequency = 1.0E09
         self.my_pm = LB5908A()
@@ -43,17 +47,28 @@ class App(tk.Tk):
         self.isreading = False
         self.__setup__()
         
-        
-
     def __setup__(self):
+        #Fill with description of Power Sensor
         self.info_label.config(text=self.my_pm.description)
-        self.my_pm.prepare_to_fetch(self.pm_frequency)
+        #Prepare start/stop button
         if not self.isreading:
             self.read_button.config(text="Start")
         else:
             self.read_button.config(text="Stop")
-
+        #Fill Freq List
+        for freq in FREQS:
+            self.list_freq.insert(tk.END, freq)
+        #Prepare to fetch values from Power Sensor
+        self.my_pm.prepare_to_fetch(self.pm_frequency)
         
+    def callback_list_freq(self, event):
+        """Callback function when a frequency is select"""
+        self.pm_frequency = FREQS[self.list_freq.curselection()[0]] *10E06
+        #print(self.pm_frequency)
+        #if reading, stop read and setting with new freq
+        if self.isreading:
+            self.start_stop_read()
+        self.my_pm.prepare_to_fetch(self.pm_frequency)
 
     def start_stop_read(self):
         """Read Button action"""
@@ -67,13 +82,9 @@ class App(tk.Tk):
             self.isreading = False
 
     def read(self):
+        """Read the Power Meter sensor in dBm using Fetch method, use 2 digits for precision"""
         self.isreading = True
         pm_read = f'{self.my_pm.fetch():.2f} dBm'
         self.canvas.itemconfig(self.read_text,text=pm_read)
         self.task_id = self.after(300, self.read)
-        
-    def show_info(self):
-        """Info Button action"""
-        pass
-
-    
+ 
