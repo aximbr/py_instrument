a9f534e88e51e7fb4e3e515ca1144b9d8f4c03ab
Jose Leitao <jose.p.leitao@gmail.com>
2024-04-11 21:56:47 +0100

    main update

--- prog_04/main_01.py
+++ prog_04/main_01.py
@@ -1,5 +1,15 @@
+#!/usr/bin/python3
 """Main Application"""
+
 from pm_gui import App
+import os, sys
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+if os.getenv('DISPLAY') == None:
+    os.environ['DISPLAY'] = ":0.0"
 
 if __name__ == "__main__":
     app = App()
